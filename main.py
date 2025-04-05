0dcfb995c9b06a08a27c57c0563e720fcc430a54
matheusf8 <matheuslobofranco@gmail.com>
2025-04-05 00:59:44 -0300

    adicionado tarefas

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 
+
+#tarefa e: fazer uma segunda tela e nessa tela guardar suas anotaçoes por enquanto so isso!
+
+
+
 class LoginApp:
     def __init__(self, root):
         self.root = root
